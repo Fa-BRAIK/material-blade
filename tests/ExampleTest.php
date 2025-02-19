2ac4c12912c5fe9a215cfaa3de1a66f1e251da63
Fa-BRAIK <36890379+Fa-BRAIK@users.noreply.github.com>
2025-02-19 16:07:40 +0000

    Fix styling

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 final class ExampleTest extends TestCase
 {
-    public function testTrueIsTrue(): void
+    public function test_true_is_true(): void
     {
         $this->assertEquals(2, 1 + 1);
     }
